2253915075e2ac04b59f6024ad62317c5d81ebf6
alexeve05 <156932121+alexeve05@users.noreply.github.com>
2026-04-03 14:06:59 -0400

    Follow single thread example for multi thread code

--- TaskSimulation.java
+++ TaskSimulation.java
@@ -2,18 +2,22 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class TaskSimulation implements Callable<Long> {
-    final long totalPoints;
+    final long totalPoints; // total # of random points
+    // constructor to initialize totalPoints for task
 
     public TaskSimulation(long totalPoints) {
         this.totalPoints = totalPoints;
     }
 
+    // performs monte carlo simulation
     @Override
+    // when the task is submitted to executorService
     public Long call() {
         long insideCircle = 0;
-        for (long i = 0; i < totalPoints; i++) {
+        for (long i = 0; i < totalPoints; i++) { // check whether they fall inside or out of circle
             double x = ThreadLocalRandom.current().nextDouble(-1.0, 1.0);
             double y = ThreadLocalRandom.current().nextDouble(-1.0, 1.0);
+            // check if point lies inside circle
             if (x * x + y * y <= 1.0) {
                 insideCircle++;
             }
